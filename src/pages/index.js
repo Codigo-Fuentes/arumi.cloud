47d56ab027d46834a97f300e91ccac3c8dcf60fd
fuentes <murray.r.adam@gmail.com>
2019-01-23 21:57:54 -0700

    added video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, } from 'gatsby'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 import './index.css'
+import backgroundVideo from "../images/arumi-background.mp4"
 
 const IndexPage = () => (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      {/* <video src={backgroundVideo}/> */}
+      <video src={backgroundVideo} autoPlay muted loop id="background"/>
     </Layout>
 )
 
