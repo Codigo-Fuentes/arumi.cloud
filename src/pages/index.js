3799ad49b18fb343876975c265e67cf7ee5367b2
fuentes <murray.r.adam@gmail.com>
2019-01-24 12:00:51 -0700

    arumi logo added

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,15 @@ import Image from '../components/image'
 import SEO from '../components/seo'
 import './index.css'
 import backgroundVideo from "../images/arumi-background.mp4"
+import arumiLogo from "../images/arumi-outlined.svg"
 
 const IndexPage = () => (
     <Layout>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      {/* <video src={backgroundVideo}/> */}
       <video src={backgroundVideo} autoPlay muted loop id="background-video"/>
+      <div className="logo-container-shows">
+        <img src={arumiLogo}/>
+      </div>
     </Layout>
 )
 
